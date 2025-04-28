566e61fd5067f06a65bd4242736d54ac17c7fca5
Alex M <alex.martin@cabinetoffice.gov.uk>
2025-04-28 10:07:34 +0100

    odp

--- examples/datasets/open_digital_planning.py
+++ examples/datasets/open_digital_planning.py
@@ -1,14 +1,49 @@
 import requests
 
-data_url = 'https://www.planning.data.gov.uk/entity/'
-def get_entity(id):
-    response = requests.get(data_url + str(id) + '.json')
+data_url = 'https://www.planning.data.gov.uk/entity'
+dataset_url = 'https://www.planning.data.gov.uk/dataset'
+
+
+def list_datasets():
+    """
+    List all available datasets.
+    
+    Returns:
+        dict: JSON response containing all datasets
+    """
+    response = requests.get(dataset_url + '.json')
     if response.status_code != 200:
-        raise Exception('Entity json not found')
+        raise Exception('Failed to retrieve datasets')
     return response.json()
 
-def get_geojson(id):
-    response = requests.get(data_url + str(id) + '.geojson')
-    if response.status_code != 200:
-        raise Exception('Entity geojson not found')
-    return response.json()
+def get_all_entities_from_dataset(dataset_id, limit=100):
+    """
+    Get all entities from a specified dataset.
+    
+    Args:
+        dataset_id (str): ID of the dataset
+        limit (int, optional): Maximum number of results to return. Defaults to 100.
+        
+    Returns:
+        list: List of entities in the dataset
+    """
+    params = {
+        'dataset': dataset_id,
+        'limit': limit
+    }
+    return search_entities(params)
+
+# Example usage
+if __name__ == "__main__":
+    # Example of listing all available datasets
+    print("\nListing all datasets...")
+    datasets = list_datasets()
+    dataset_names=[dataset['name'] for dataset in datasets['datasets']]
+    print(f"Found {len(datasets.get('datasets', []))} datasets")
+    # Example of getting all entities from a dataset
+    # Replace 'example-dataset' with an actual dataset ID
+    print("Getting entities from dataset...")
+    dataset_entities = get_all_entities_from_dataset('article-4-direction')
+    print(f"Found {len(dataset_entities.get('entities', []))} entities")
+    
+    
