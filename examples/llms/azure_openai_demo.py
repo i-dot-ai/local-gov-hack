f18beb5dbc94f2986f42f21e0e7b82eed0895b2a
Oliver Normand <oliver.normand@cabinetoffice.gov.uk>
2025-04-29 13:58:18 +0100

    bumped the model and api version

--- examples/llms/azure_openai_demo.py
+++ examples/llms/azure_openai_demo.py
@@ -2,8 +2,8 @@ import os
 from openai import AzureOpenAI
 
 # Ensure your environment variables are set - these will have been provided as part of the hackathon
-openai_model = os.environ.get("AZURE_OPENAI_MODEL", "gpt-4.1")
-api_version = os.environ.get("OPENAI_API_VERSION", "2024-08-01-preview")
+openai_model = os.environ.get("AZURE_OPENAI_MODEL", "gpt-4.1-mini")
+api_version = os.environ.get("OPENAI_API_VERSION", "2024-12-01-preview")
 azure_endpoint = os.environ.get("AZURE_OPENAI_ENDPOINT")
 api_key = os.environ.get("AZURE_OPENAI_KEY")
 
